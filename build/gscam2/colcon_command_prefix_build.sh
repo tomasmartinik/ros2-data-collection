b5856c289de7a93db52b21c07918c720a6ab988e
Tomáš Martiník <tomas@martinik.eu>
2025-03-12 08:56:57 +0100

    build folder deleted

--- build/gscam2/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/workspaces/ros2-devcontainer-example-ws/install/ros2_shared/share/ros2_shared/package.sh"
--- build/ros2_shared/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/ros2-devcontainer-example-ws/src/ros2_shared
-# Build directory: /workspaces/ros2-devcontainer-example-ws/build/ros2_shared
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
